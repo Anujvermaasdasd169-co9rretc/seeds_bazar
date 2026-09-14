846ad1570442cd3f2b239c327a1c4aa991a5ad01
anujverma123 <128671639+anujverma123@users.noreply.github.com>
2026-09-14 18:19:22 +0530

    animation vidio

--- resources/views/components/storefront-banner.blade.php
+++ resources/views/components/storefront-banner.blade.php
@@ -13,15 +13,12 @@
             <div class="hero-slide__shade"></div>
         </article>
     </div>
-    {{-- <div class="hero-slider__controls">
-        <button type="button" class="hero-slider__arrow" data-slider-prev aria-label="Previous banner">←</button>
+    <div class="hero-slider__controls">
         <div class="hero-slider__dots" role="tablist" aria-label="Choose a banner">
             <button type="button" class="hero-slider__dot is-active" data-slider-dot="0" role="tab" aria-label="Show banner 1" aria-selected="true"></button>
             <button type="button" class="hero-slider__dot" data-slider-dot="1" role="tab" aria-label="Show banner 2" aria-selected="false"></button>
             <button type="button" class="hero-slider__dot" data-slider-dot="2" role="tab" aria-label="Show banner 3" aria-selected="false"></button>
         </div>
-        <button type="button" class="hero-slider__arrow" data-slider-next aria-label="Next banner">→</button>
-    </div> --}}
-    <div class="hero-slider__progress" aria-hidden="true"><span></span></div>
+    </div>
 </section>
 <script src="{{ asset('js/banner-slider.js') }}?v={{ @filemtime(public_path('js/banner-slider.js')) ?: time() }}" defer></script>
